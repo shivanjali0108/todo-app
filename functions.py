1f29ec365c2522f21d1f092f5100a300fd395f9a
shivanjali0108 <shivanjalidesai59@gmail.com>
2026-06-30 08:03:58 +0530

    Refactor get_todos and write_todos functions
    Updated function parameters for consistency and added docstrings.

--- functions.py
+++ functions.py
@@ -1,12 +1,20 @@
 FILEPATH = "todos.txt"
 
 
-def get_todos(filepath=FILEPATH):
-    with open(filepath, 'r') as file:
+def get_todos(file_path=FILEPATH):
+    """
+    Read the to-do items from the text file
+    and return them as a list.
+    """
+    with open(file_path, "r") as file:
         todos_local = file.readlines()
+
     return todos_local
 
 
-def write_todos(todos_arg, filepath=FILEPATH):
-    with open(filepath, 'w') as file:
+def write_todos(todos_arg, file_path=FILEPATH):
+    """
+    Write the list of to-do items to the text file.
+    """
+    with open(file_path, "w") as file:
         file.writelines(todos_arg)
